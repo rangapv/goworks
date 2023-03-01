0ed97e766391af9d64931d58cc041a7f534b6a88
rangapv <rangapv@yahoo.com>
2023-03-01 13:22:35 +0000

    function to add imports in a for loop

--- gobash.sh
+++ gobash.sh
@@ -8,7 +8,6 @@ goc1=`which go`
 goc1s="$?"
 goc2=`go version`
 goc2s="$?"
-
 if [[ ($goc1s -eq 0) && ($goc2s -eq 0) ]]
 then
 	echo "Current `(go version)` in this system"
@@ -21,13 +20,30 @@ fi
 
 makfile () {
 
-imp="$1"
-
+imp="$@"
 `echo "package main" >> $HOME/$mod1/$mod1.go`
-`echo "import \"$imp\"" >> $HOME/$mod1/$mod1.go`
+`echo "import ("  >> $HOME/$mod1/$mod1.go`
+
+impfunc $imp 
+
+#`echo "\"$imp\"" >> $HOME/$mod1/$mod1.go`
+`echo ")" >> $HOME/$mod1/$mod1.go`
 `funcmain >> $HOME/$mod1/$mod1.go`
 
 }
+
+impfunc () {
+
+arg="$#"
+arg1="$@"
+i=0
+for i in "$@"
+do
+	`echo "   \"$i\"" >> $HOME/$mod1/$mod1.go`
+done
+
+}
+
 
 funcmain () {
 
@@ -43,9 +59,8 @@ echo " THis is a go skeletal program generator"
 echo " Enter the Module name (a.k.a example/hello)"
 read mod
 mod1=`(echo "$mod" | awk '{ split($0,a,"/") ; print a[2] }')`
-echo "mod1 is $mod1"
 d1=`mkdir $HOME/$mod1`
 cd1=`cd $HOME/$mod1;go mod init $mod;touch "$mod1.go"`
 
 
-makfile fmt
+makfile fmt rpc
